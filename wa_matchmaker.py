d04d8902e04ff9c93b21911341a06bc521b491a5
William Brantley <web254@nyu.edu>
2016-11-12 02:04:22 -0500

    initial commit. Untested skeleton code

---
+++ wa_matchmaker.py
@@ -0,0 +1,38 @@
+from dating import utils
+import sys
+import socket
+import numpy as np
+
+def guess_weights(scores, weights):
+    """Guess the weights of the Person's attributes based on past scores
+    and weights of those scores, included as arguments. Return the your 
+    guess as a numpy array (np.array)"""
+
+def main():
+    port = int(sys.argv[1])
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect(('localhost', port))
+    num_of_attributes = int(socket.recv(4))
+
+    scores = []
+    weights = []
+    # Get training data from architects
+    for _ in range(20):
+        training_sample = socket.recv(8 + 2 * num_of_attributes)
+        score = float(training_sample[:7])
+        scores.append(score)
+        sample_weights = [float(attr.strip()) for attr in training_sample[8:].split(',')]
+        weights.append(sample_weights)
+
+    for _ in range(20):
+        guess = guess_weights(scores, weights)
+        socket.sendall(floats_to_msg4(guess))
+        score = float(socket.recv(8))
+
+        scores.append(score)
+        weights.append(guess)
+
+    socket.close()
+
+if __name__=="__main__":
+    main()
---
+++ wa_person.py
@@ -0,0 +1,46 @@
+from dating import utils
+import sys
+import socket
+import numpy as np
+
+def generate_weights(num_of_attributes):
+    """This function should generate a vector of size num_of_attributes
+    that follow the rules of the game, i.e. all positive values should sum
+    to 1, and there must be at least 1 positive value. Similarly, all negative
+    values should sum to -1, and there should be at least 1 negative value
+    
+    The returned type should be a numpy array (np.array)"""
+
+def adjust_weights(initial_weights, guess):
+    """Adjust the initial weights provided within the rules stated in the game,
+    i.e. no more than 5% of them can change, and those changed can't change by 
+    more than 20%
+    
+    The returned type should be a numpy array (np.array)"""
+
+
+
+def main():
+    port = int(sys.argv[1])
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect(('localhost', port))
+    num_of_attributes = int(socket.recv(4))
+
+    initial_weights = generate_weights(num_of_attributes)
+
+    socket.sendall(floats_to_msg2(initial_weights))
+    # Send ideal (positive) candidate positions
+    socket.sendall(candidate_to_msg(initial_weights > 0))
+    # Send anti-ideal (negative) candidate positions
+    socket.sendall(candidate_to_msg(initial_weights < 0))
+
+    for i in range(20):
+        matchmaker_guess = socket.recv(8 * num_of_attributes)
+        matchmaker_guess = [float(attr.strip()) for attr in matchmaker_guess.split(',')]
+        adjusted_weights = adjust_weights(initial_weights, matchmaker_guess)
+        socket.sendall(floats_to_msg2(adjusted_weights))
+
+    socket.close()
+
+if __name__=="__main__":
+    main()
